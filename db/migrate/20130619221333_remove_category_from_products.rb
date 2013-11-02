7a8fa514584f135e2fbf7727225722a519dc9733
ansonwolfe <ansonwolfe@gmail.com>
2013-11-02 16:58:30 -0700

    testing push

--- db/migrate/20130619221333_remove_category_from_products.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCategoryFromProducts < ActiveRecord::Migration
-  def up
-    remove_column :products, :category
-  end
-
-  def down
-    add_column :products, :category, :integer
-  end
-end
